d29772870a36c24d9149e8ae2a33073a4c01c89b
Dennis Quiguire <instagram.24@outlook.com>
2021-06-24 12:09:07 -0500

    Trabajo en Clase

--- programming/01_Projects/index.js
+++ programming/01_Projects/index.js
@@ -1,3 +1,4 @@
+
 //node index.js
 const express = require('express');
 
@@ -8,6 +9,34 @@ const app = express()
 // endpoint 
 app.get('/',(req, res) => {
     res.send("Hola mundo con ExpressJS")
+})
+
+app.get("/saludo",(req,res)=>{
+    //req.query
+    //console.log(req.query);
+    //obtenemos la query del objeto request
+    
+    // const{query}=req;
+    const{query:{nombre,apellido}}=req;
+    
+
+    //const nombre=req.query.nombre
+    //const apellido=req.query.apellido
+    
+    res.json({
+        //saludo:`hola soy ${query.nombre} ${query.apellido}`
+        saludo:`hola soy ${nombre} ${apellido}`
+    })
+})
+
+app.get('/saludo/:nombre', (req, res) =>{
+    //http://localhost:3000/saludo/Dennis
+    const { params: { nombre } } = req
+    //console.log(req.params);
+    res.json({
+        // nombre: nombre = req.params.nombre
+        nombre: nombre
+    })
 })
 
 app.listen(3000, ()=>{
---
+++ programming/01_Projects/routes/index.js
@@ -0,0 +1,24 @@
+//node index.js
+const {Router}=require('express')
+//inicializando Router
+const router=Router()
+
+//creacion de los endpoint
+
+
+//Exportamos las rutas
+module.exports.RouterIndex=router
+
+/*
+module={
+    exports:{
+        RouterIndex:router
+    }
+}
+*/
+
+
+
+
+
+
