f6b154c9c820896d7d7f82a5ef8d58106c90b3b5
Dennis Quiguire <instagram.24@outlook.com>
2021-06-25 12:07:28 -0500

    Trabajo en Clase

--- programming/01_Projects/index.js
+++ programming/01_Projects/index.js
@@ -1,43 +1,22 @@
 
-//node index.js
+const { Router } = require('express');
 const express = require('express');
-
-
 //genero una aplicacion express
 
 const app = express()
-// endpoint 
-app.get('/',(req, res) => {
-    res.send("Hola mundo con ExpressJS")
-})
+// routes
+const { RouterIndex } = require('./routes/index')
+app.use("/", RouterIndex)
+app.use("/user", RouterIndex)
+app.use("/user/data", RouterIndex)
 
-app.get("/saludo",(req,res)=>{
-    //req.query
-    //console.log(req.query);
-    //obtenemos la query del objeto request
-    
-    // const{query}=req;
-    const{query:{nombre,apellido}}=req;
-    
-
-    //const nombre=req.query.nombre
-    //const apellido=req.query.apellido
-    
-    res.json({
-        //saludo:`hola soy ${query.nombre} ${query.apellido}`
-        saludo:`hola soy ${nombre} ${apellido}`
-    })
-})
-
-app.get('/saludo/:nombre', (req, res) =>{
-    //http://localhost:3000/saludo/Dennis
-    const { params: { nombre } } = req
-    //console.log(req.params);
-    res.json({
-        // nombre: nombre = req.params.nombre
-        nombre: nombre
-    })
-})
+/*
+        "/"+"/"="//" => "/"
+        "/"+"saludo" = "//Saludo" => "/saludo"
+        "/"+ "'/saludo/:nombre'" = //saludo/:nombre => "/saludo/:nombre"
+        "/user" + "/saludo" = "/user/saludo"
+        "user/data" + "/saludo" = "/user/data/saludo"
+*/
 
 app.listen(3000, ()=>{
     console.log("Servidor escuchando en htpp://localhost:3000");
--- programming/01_Projects/routes/index.js
+++ programming/01_Projects/routes/index.js
@@ -1,24 +1,41 @@
-//node index.js
-const {Router}=require('express')
-//inicializando Router
-const router=Router()
+//traemos el paquete
+
+const { Router } = require('express');
+
+//inicializamos el router
+
+const router = Router()
 
 //creacion de los endpoint
+router
+    .get("/",(req, res) => {
+        //http://localhost:3000/
+    res.send("Hola mundo con ExpressJS");
+    })
+    .get("/saludo",(req, res) =>{
+
+        const { query: {nombre, apellido} } = req;
+        res.json({
+            saludo:` Hola soy ${nombre} ${apellido}`,
+            //http://localhost:3000/saludo?nombre=Dennis&apellido=Quiguire
+        });
+    })
+    .get('/saludo/:nombre', (req, res) =>{
+        const { params: { nombre } } = req
+        console.log(req.params);
+        res.json({
+            //http://localhost:3000/saludo/:Dennis
+            nombre: nombre,
+        })
+    })
 
 
-//Exportamos las rutas
-module.exports.RouterIndex=router
 
-/*
-module={
-    exports:{
-        RouterIndex:router
-    }
-}
-*/
+//exportamos la rutas
+module.exports.RouterIndex = router
 
-
-
-
-
-
+// module = {
+//     exports: {
+//         RouterIndex: router
+//     }
+// }
