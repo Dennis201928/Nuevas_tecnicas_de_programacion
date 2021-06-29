aabd50474168a7656e7d59a5299fe7ba9c8c3702
Dennis Quiguire <instagram.24@outlook.com>
2021-06-29 11:53:15 -0500

    Trabajo en Clase 29/06/2021

---
+++ programming/01_Projects/middlewares/DataValidator.js
@@ -0,0 +1,29 @@
+//            DataValidator.js
+
+ //const{User}=require(`../lib/Shema/User`)
+
+function datavalidator(check="query",schema){
+
+    return (req,res,next)=>{
+        var data=req[check]
+        const{error,value}=schema.validate(data)
+
+        if (error) {
+            res.status(406).json({
+                msg:error.details[0].message,
+            })
+        }else{
+            req[check]=value
+            next()
+        }
+
+    }
+}
+
+/*
+function datavalidator(req, res, next) {
+  req.pruebaMiddle = "Hola mundo con middleware";
+  next();
+}
+*/
+module.exports.datavalidator = datavalidator;
